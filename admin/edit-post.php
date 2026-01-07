c178d766e072c7f3442e128e7bb6841ada8b435e
Yman <64341841+Yman14@users.noreply.github.com>
2026-01-07 18:44:58 +0800

    validate id url

--- admin/edit-post.php
+++ admin/edit-post.php
@@ -6,3 +6,12 @@ if (!isset($_SESSION['admin_id'])) {
     header('Location: login.php');
     exit;
 }
+
+require_once '../includes/db.php';
+
+//validate id url
+if (!isset($_GET['id']) || !is_numeric($_GET['id'])) {
+    die('Invalid post ID');
+}
+
+$postId = (int) $_GET['id'];
