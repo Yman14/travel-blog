1c7bf677aa5e037eda4ba32118867dfbda445aa8
Yman <64341841+Yman14@users.noreply.github.com>
2026-01-07 18:36:27 +0800

    fetch posts

--- admin/posts.php
+++ admin/posts.php
@@ -1,7 +1,21 @@
 <?php
 session_start();
 
+//access protection
 if (!isset($_SESSION['admin_id'])) {
     header('Location: login.php');
     exit;
 }
+
+require_once '../includes/db.php';
+
+//fetch datas
+$sql = "SELECT posts.id, posts.title, posts.status, posts.created_at, categories.name AS category
+        FROM posts
+        JOIN categories ON posts.category_id = categories.id
+        ORDER BY posts.created_at DESC";
+
+$stmt = $pdo->prepare($sql);
+$stmt->execute();
+$posts = $stmt->fetchAll(PDO::FETCH_ASSOC);
+?>
