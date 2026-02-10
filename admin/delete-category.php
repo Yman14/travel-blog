7844cfee0e437065b796bcea1c0d850d7efcb5b1
Yman <64341841+Yman14@users.noreply.github.com>
2026-02-10 14:44:35 +0800

    check csrf token

--- admin/delete-category.php
+++ admin/delete-category.php
@@ -3,7 +3,12 @@ require_once '../includes/config.php';
 require_once __DIR__ . '/includes/auth.php';
 require_once __DIR__ . '/includes/admin-header.php';
 
-if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+if (
+    $_SERVER['REQUEST_METHOD'] !== 'POST' ||
+    !hash_equals($_SESSION['csrf_token'], $_POST['csrf_token'] ?? '')
+) {
+    // http_response_code(403);
+    // exit;
     $_SESSION['flash_error'] = "Delete not successful.";
     header('Location:' . BASE_URL .  'admin/categories');
     exit;
