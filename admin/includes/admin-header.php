d53ee29f6624a460b772768dc1be7e5c67edbbd5
Yman <64341841+Yman14@users.noreply.github.com>
2026-02-15 16:55:30 +0800

    element for more btn

--- admin/includes/admin-header.php
+++ admin/includes/admin-header.php
@@ -16,7 +16,13 @@
             <a href="<?=BASE_URL?>admin/posts">Posts</a>
             <a href="<?=BASE_URL?>admin/categories">Categories</a>
         </nav>
-        <div class="log"><a href="<?=BASE_URL?>admin/logout">Logout</a></div>
+        <div class="log"><a href="<?=BASE_URL?>admin/logout" class='logout'>Logout</a></div>
+        <div class="more">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                <title>More</title>
+                <path d="M12,16A2,2 0 0,1 14,18A2,2 0 0,1 12,20A2,2 0 0,1 10,18A2,2 0 0,1 12,16M12,10A2,2 0 0,1 14,12A2,2 0 0,1 12,14A2,2 0 0,1 10,12A2,2 0 0,1 12,10M12,4A2,2 0 0,1 14,6A2,2 0 0,1 12,8A2,2 0 0,1 10,6A2,2 0 0,1 12,4Z" />
+        </svg>
+        </div>
     </div>
 </header>
 
